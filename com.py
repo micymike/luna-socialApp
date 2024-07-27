d93fbe0b387897b56455e4bf7e478a276eaed3b9
micymike <mosesmichael878@gmail.com>
2024-07-27 23:57:18 +0300

    "Moved recreate_tables function from com.py to app.py and removed redundant function definition in com.py"

--- com.py
+++ com.py
@@ -1,8 +1,5 @@
 from app import app, db
 
-@app.route('/recreate_tables')
-def recreate_tables():
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
-    return 'Tables recreated successfully!'
+with app.app_context():
+    db.create_all()
+    print("Tables created successfully!")
